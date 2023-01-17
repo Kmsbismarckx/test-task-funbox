d3dd4d2b4fb152127bdaa8a6e1467bddc29cdf99
Bismarck <klimentmaslennikov@gmail.com>
2023-01-17 22:36:53 +0300

    finish

--- src/components/MainItem/MainItem.jsx
+++ src/components/MainItem/MainItem.jsx
@@ -23,34 +23,50 @@ const MainItem = ({ id }) => {
 
   return (
     <div
-      className={`main__item ${isActive ? "main__item_active" : ""} ${
-        !isAvailable ? "main__item_not-available" : ""
+      className={`main__item-container ${
+        isActive ? "main__item-container_active" : ""
+      } ${!isAvailable ? "main__item-container_not-available" : ""} ${
+        toggleText ? "main__item-container_hover" : ""
       }`}
-      onClick={onActive}
-      onMouseOver={onHover}
-      onMouseOut={onOut}
     >
-      <div className="item__header">
-        <div className="item__corner" />
-        <p className="header__name">
-          <span>
-            {toggleText ? "Котэ не одобряет?" : "Сказачное заморское яство"}
-          </span>
-        </p>
-      </div>
-      <div className="item__description">
-        <h1 className="description__name">Нямушка</h1>
-        <p className="description__composition">с {composition}</p>
-        <p className="description__quantity">
-          {plural(quantity, "%d порция", "%d порции", "%d порций")}
-        </p>
-        <p className="description__present">
-          {plural(present, "мышь", "%d мыши", "%d мышей")} в подарок
-        </p>
-        <img className="item__img" src={`${publicUrl}/media/cat.png`} alt="" />
-        <div className="item__circle">
-          <span>{weight.toString().replace(".", ",")}</span>
-          <div className="item__measure">кг</div>
+      <div
+        className={`main__item `}
+        onClick={onActive}
+        onTouchStart={onActive}
+        onMouseEnter={onHover}
+        onMouseLeave={onOut}
+      >
+        <div className="item__header">
+          <div
+            className="item__corner"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          />
+          <p className="header__name">
+            <span>
+              {toggleText ? "Котэ не одобряет?" : "Сказачное заморское яство"}
+            </span>
+          </p>
+        </div>
+        <div className="item__description">
+          <h1 className="description__name">Нямушка</h1>
+          <p className="description__composition">с {composition}</p>
+          <p className="description__quantity">
+            {plural(quantity, "%d порция", "%d порции", "%d порций")}
+          </p>
+          <p className="description__present">
+            {plural(present, "мышь", "%d мыши", "%d мышей")} в подарок
+          </p>
+          <img
+            className="item__img"
+            src={`${publicUrl}/media/cat.png`}
+            alt=""
+          />
+          <div className="item__circle">
+            <span>{weight.toString().replace(".", ",")}</span>
+            <div className="item__measure">кг</div>
+          </div>
         </div>
       </div>
       {isActive ? (
